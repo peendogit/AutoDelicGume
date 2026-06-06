eefb0c3e79a2a36974c06849faea2d9a93cec6e3
peendogit <peendo@gmail.com>
2026-06-06 11:54:34 +0200

    Add files via upload

--- src/modules/Gume.jsx
+++ src/modules/Gume.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
 import { api, promjerDisp, tipLbl, statusLbl, timeAgo, resizeImage, EMPTY_GUMA, PROMJER_OPTIONS, SIRINA_OPTIONS, VISINA_OPTIONS } from '../utils.js';
 import { Icons, ErrorBoundary, ComboBox, useImageUpload, Lightbox, ImgUploadUI, Pagination } from '../components/index.jsx';
+import PremjestanjeWidget from './Premjestanje.jsx';
 
 function GumeModul({user,showToast,gume,setGume,police,magacini,loadPolice,lightbox,setLightbox,quickAdd,onQuickAddDone}){
   const isAdmin=user.role==='admin';
